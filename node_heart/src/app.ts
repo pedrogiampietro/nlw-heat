6c3e821d4c6393601148dff422971cfb76bc0fc5
Pedro Giampietro Reis <pedro.reis@cejam.local>
2021-11-02 10:45:47 -0300

    auth user and recive code github.

---
+++ node_heart/src/app.ts
@@ -0,0 +1,25 @@
+import 'dotenv/config';
+import express from 'express';
+
+import { router } from './routes';
+
+const app = express();
+
+app.use(express.json());
+app.use(router);
+
+app.get('/github', (request, response) => {
+  const githubID = process.env.GITHUB_CLIENT_ID;
+
+  response.redirect(
+    `https://github.com/login/oauth/authorize?client_id=${githubID}`
+  );
+});
+
+app.get('/signin/callback', (request, response) => {
+  const { code } = request.query;
+
+  return response.json(code);
+});
+
+app.listen(4000, () => console.log(`🚀 Server is running on PORT 4000!`));
